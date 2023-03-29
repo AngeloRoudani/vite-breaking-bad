929ea930d894607bda5903b03f68d58f39a969e4
AngeloRoudani <ange.rouda@gmail.com>
2023-03-29 18:09:18 +0200

    modifiche css

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -3,6 +3,7 @@
         <img :src="image" :alt="name">
         <h4>{{name}}</h4>
         <div>{{type}}</div>
+        
     </div>
 </template>
 
@@ -26,9 +27,24 @@
 </script>
 
 <style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+
+    .card {
+        width: calc(100% - 1rem) ;
+        background-color: $bg-primary;
+        height: 400px;
+        margin-bottom: 1rem;
+    }
 
     img {
-        width: 100%;
+        margin-right: 10px;
+        width: 110%;
         padding: 0 1rem;
+    }
+
+    h4 {
+        color: $color-primary;
+        text-transform: uppercase;
+        padding: 5px;
     }
 </style>
